2ee81d87a1f95f463c33c11e4768198cf5ea29e7
nader <esmailjafarpour29@gmail.com>
2023-04-29 19:24:57 +0330

    create project redux

---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {increase} from "./redux/counter/counterAction";
+import {connect} from "react-redux";
+
+const Counter = (props) => {
+     return (
+          <div>
+               <h2>Counter{props.counter}</h2>
+               <button onClick={props.increase}>Increase</button>
+          </div>
+     );
+}
+
+const mapStateToProps = (state) => {
+
+     return{
+          counter : state.counter
+     }
+
+}
+
+
+const mapDispatchToProps = (dispatch) => {
+     return {
+          increase : () => dispatch(increase())
+     }
+}
+
+export default connect(mapStateToProps , mapDispatchToProps) (Counter);
