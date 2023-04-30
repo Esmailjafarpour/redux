c8d981cdfabc631ef17fd28f495ac808e050434e
nader <esmailjafarpour29@gmail.com>
2023-04-30 14:53:10 +0330

    New method of receiving state and dispatch

--- src/Counter.js
+++ src/Counter.js
@@ -1,29 +1,52 @@
+// import React from 'react';
+// import {increase} from "./redux/counter/counterAction";
+// import {connect} from "react-redux";
+
+// const Counter = (props) => {
+//      return (
+//           <div>
+//                <h2>Counter{props.counter}</h2>
+//                <button onClick={props.increase}>Increase</button>
+//           </div>
+//      );
+// }
+
+// const mapStateToProps = (state) => {
+
+//      return{
+//           counter : state.counter
+//      }
+
+// }
+
+
+// const mapDispatchToProps = (dispatch) => {
+//      return {
+//           increase : () => dispatch(increase())
+//      }
+// }
+
+// export default connect(mapStateToProps , mapDispatchToProps) (Counter);
+
+//method Two
 import React from 'react';
 import {increase} from "./redux/counter/counterAction";
-import {connect} from "react-redux";
+import {useSelector , useDispatch} from "react-redux";
+
 
 const Counter = (props) => {
+
+     const state = useSelector(state => state);
+     const dispatch = useDispatch();
+     
      return (
           <div>
-               <h2>Counter{props.counter}</h2>
-               <button onClick={props.increase}>Increase</button>
+               <h2>Counter{state.counter}</h2>
+               <button onClick={() => dispatch(increase())}>Increase</button>
           </div>
      );
 }
 
-const mapStateToProps = (state) => {
-
-     return{
-          counter : state.counter
-     }
-
-}
 
 
-const mapDispatchToProps = (dispatch) => {
-     return {
-          increase : () => dispatch(increase())
-     }
-}
-
-export default connect(mapStateToProps , mapDispatchToProps) (Counter);
+export default Counter;
